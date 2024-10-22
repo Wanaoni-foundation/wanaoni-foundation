eb446418761081d2729ccae3b62b02465501025d
wanaoni mike <misikomichael@gmail.com>
2024-10-22 19:49:29 +0300

    Set up Rust project with correct Cargo.toml

--- wanaonifoundation/myrustproject/src/main.rs
+++ wanaonifoundation/myrustproject/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     println!("Hello, Wanaoni Foundation!");
 }
+git add .
+git commit -m "Set up Rust project with correct Cargo.toml"
+git push origin master
+fn main() {
+    println!("Hello, Wanaoni Foundation!");
+}
 fn main() {
     println!("Hello, world!");
 }
